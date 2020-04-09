1797a0d718f3d59e147157a79738f7aafbe17ff6
Jon Tran <trungt8998@gmail.com>
2020-04-09 10:56:33 -0500

    remove ad panel from elfsight in customer review

---
+++ src/assets/js/customer-review.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $(".eagr-reviews-skeleton-container.jsx-1473416917 a").last().hide();
+});
+
+// $(document).ready(function(){
+//   $(".customer-review").hide();
+// })
