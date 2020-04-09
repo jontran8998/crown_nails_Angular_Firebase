8bef5f6010d82af4f90b95fcf7e103b63616c333
Jon Tran <trungt8998@gmail.com>
2020-04-09 15:00:30 -0500

    Remove Ad banner on customer review

--- src/assets/js/customer-review.js
+++ src/assets/js/customer-review.js
@@ -1,7 +1,18 @@
-$(document).ready(function(){
-  $(".eagr-reviews-skeleton-container.jsx-1473416917 a").last().hide();
+// $(document).ready(function(){
+//   $(".eagr-reviews-skeleton-container.jsx-1473416917 a").last().hide();
+//   console.log('the code is running');
+// });
+
+const removeAdd = function(){
+  $(document).ready(function(){
+    $(".eagr-reviews-skeleton-container.jsx-1473416917 a").last().hide();
+    localStorage.clear();
+    console.log('the code is running');
+  });
+}
+
+setTimeout(removeAdd(), 5000);
+$( window ).on( "load", function() {
+  console.log( "window loaded" );
 });
 
-// $(document).ready(function(){
-//   $(".customer-review").hide();
-// })
