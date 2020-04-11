95e5489c0ccb1e29e67311d2a9094b90f9ee9a32
Jon Tran <trungt8998@gmail.com>
2020-04-11 10:56:40 -0500

    refactory js code

--- src/assets/js/google-review.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function() {
-  $("#google-reviews").googlePlaces({
-      placeId: 'ChIJOy6Wkc516lIR8rq01wwdAeA',
-      render: ['reviews'],
-      min_rating: 5,
-      max_rows: 0
-  });
-});
