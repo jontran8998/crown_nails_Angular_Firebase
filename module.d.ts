b42644fa84b825e314737d80d73df9ad34c8bc7d
Jon Tran <trungt8998@gmail.com>
2020-10-01 20:49:39 -0500

    Fix Winter Hours

---
+++ module.d.ts
@@ -0,0 +1,7 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    API_KEY: "AIzaSyDZzZgoSDUAsVTbC3_AIKMFYV3EuoIsqyA";
+    AUTH_DOMAIN: "crownnailsandspa-e8452.firebaseapp.com";
+    PROJECT_ID: "crownnailsandspa-e8452";
+  }
+}
